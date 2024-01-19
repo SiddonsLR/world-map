cf004f3eece09148b8542e47ef28a12587f808a2
965421_William <liam946994@gmail.com>
2024-01-19 17:10:52 -0600

    bug in hover

--- interactive-map/src/app/components/world-map/world-map.component.ts
+++ interactive-map/src/app/components/world-map/world-map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-world-map',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './world-map.component.css'
 })
 export class WorldMapComponent {
+countryName = '';
+countryData:any = null;
+ constructor(private apiservice:ApiService){}
+ getCounrtryData(){
+  this.apiservice.getCountryData(this.countryName).subscribe((data:any)=>{
+    this.countryData=data[1][0];
+  }
+  )
+ }
 
+ onMouseOver(event:any):void{
+  let country = event.target.id;
+  this.countryName = country;
+  this.getCounrtryData();
+ }
 }
--- interactive-map/src/app/services/api.service.ts
+++ interactive-map/src/app/services/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  countryName = '';
+  private apiUrl = 'http://api.worldbank.org/v2/country';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getCountryData(countryName: string): Observable<any> {
+    const url = `${this.apiUrl}/${countryName}?format=json`;
+    return this.http.get(url);
+  }
+
 }
